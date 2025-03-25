7d1cd5957673b7aea1d467223a8047eb0772a77a
Vancha <tjipkevanderheide90@gmail.com>
2025-03-25 15:23:29 +0100

    Add files via upload

---
+++ cryocalc/src/main.rs
@@ -0,0 +1,57 @@
+use iced::widget::{button, column, row, text, text_input};
+use iced::Element;
+
+/// This is the struct that will eventually be able to parse the expressions
+/// Through something like  ```let result = ExpressionParser::Parse("1 + 5 / (9 * 3)");```
+struct ExpressionParser {}
+
+#[derive(Default)]
+enum CalculatorMode {
+    #[default]
+    Regular,
+    Hexadecimal,
+    Binary,
+}
+
+/// This struct, holds the state of the entire application
+#[derive(Default)]
+struct CryoCalc {
+    //holds the state for our display
+    display_content: String,
+    //used to decide what to display
+    mode: CalculatorMode,
+}
+
+pub fn main() -> iced::Result {
+    iced::run("CryoCalc", CryoCalc::update, CryoCalc::view)
+}
+
+impl CryoCalc {
+    fn update(self: &mut CryoCalc, message: Message) {
+        match message {
+            //gets triggered when typing expressions in the calculators display
+            Message::DisplayContentChanged(content) => {
+                self.display_content = content;
+            }
+        }
+    }
+
+    fn view(self: &CryoCalc) -> Element<Message> {
+       match self.mode {
+           _ => {
+                column![
+                    //example, shows a text input widget w
+                    text_input("...", &self.display_content)
+                        .on_input(Message::DisplayContentChanged)
+                ].into()
+           },
+       }
+    }
+}
+
+#[derive(Debug, Clone)]
+enum Message {
+    DisplayContentChanged(String),
+    //Likely this needs some kind of button pressed message that tells us which button has been
+    //pressed?
+}
